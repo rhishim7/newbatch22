033ba4e34a97a619a423e67647f22ed5a2d89cd8
RM66633 <RM66633@POOLW42SLPC6525.ind.zensar.com>
2022-05-06 15:16:13 +0530

    Commited stock new application

--- stock-application/src/main/java/com/zensar/stockapplication/StockApplication.java
+++ stock-application/src/main/java/com/zensar/stockapplication/StockApplication.java
@@ -3,11 +3,15 @@ package com.zensar.stockapplication;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class StockApplication{
-
+public class StockApplication extends SpringBootServletInitializer{
+// Here we have added extends SpringBootServletInitializer to generate war file 
+	// also we have overridden SpringApplicationBuilder configure from it.
+	// also added packaging as war in pom.xml
 	public static void main(String[] args) {
 		SpringApplication.run(StockApplication.class, args);
 	}
@@ -16,6 +20,12 @@ public class StockApplication{
 	public ModelMapper modelMapper()
 	{
 		return new ModelMapper();
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return super.configure(builder);
 	}	
 
 }
